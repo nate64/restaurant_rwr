4357781d9ac578303d5d83fb6a7598b4c50c0f4f
Nate <nathanestes@gmail.com>
2018-09-17 16:44:00 -0600

    gett somewhere?

--- app/controllers/api/menu_items_controller.rb
+++ app/controllers/api/menu_items_controller.rb
@@ -1,23 +1,42 @@
-class Api::MenuItemsController < ApplicationController
-  before_action :set_menu, only: [:show, :update, :destroy]
-  
+class Api::ProductsController < ApplicationController
+  before_action :set_menu_items, only: [:show, :update, :destroy]
+
   def index
-    render json:Menu.all
+    render json: Menu_items.all
   end
 
   def show
+    render json: @menu_items
   end
 
-  def new
+  def create
+    menu_item = Menu_item.new(product_params)
+
+    if menu_item.save
+      render json: menu_item
+    else
+      render json: menu_item.errors, status: 422
+    end
   end
 
   def update
+    if @menu_item.update(menu_item_params)
+      render json: @menu_item
+    else
+      render json: @menu_item.errors, status: 422
+    end
   end
 
   def destroy
+    @menu_item.destroy
   end
 
-  private 
+  private
+    def set_menu_items
+      @menu_item = Menu_item.find(params[:id])
+    end
 
-
+    def menu_item_params
+      params.require(:menu_item).permit(:name, :description, :price)
+    end
 end
