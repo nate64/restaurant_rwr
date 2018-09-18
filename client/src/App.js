e14bb988af288b39b5a7ce4107aebf13b3229989
Nate <nathanestes@gmail.com>
2018-09-18 17:05:35 -0600

    help from alex

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About.js'
+import Home from './Components/Home.js'
+import MenuItem from './MenuItem.js'
+import axios from 'axios'
 
-class App extends Component {
+
+class App extends React.Component {
+  state = {
+    menus: [],
+    //editing: null //what the hell does this do?
+  }
+
+componentDidMount() { //axios data call from rails
+  axios.get('/api/menu_items')
+    .then( res => this.setState({ menus: res.data }))
+}
+
+  showMenu = () => {}
+  //show one item
+
+  addMenuItem(){}
+  //axios.post
+
+  deleteMenuItem(){}
+  
+
+
   render() {
+    const { menus } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ul>
+        { menus.map( m => 
+          <li key={m.id}>{m.name} | {m.description} | {m.price}</li>
+          )}
+      </ul>
+    )
   }
 }
 
+
+
+
+
 export default App;
+
+
+
+{/* <MenuItem
+          {...menu_item}
+          addMenuItem={this.addMenuItem}
+          deleteMenuItem={this.deleteMenuItem} */}
+        
---
+++ client/src/MenuItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MenuItem = ({
+  id,
+  name,
+  description,
+  price,
+}) => (
+  <div className="menu_item">
+    
+  </div>
+)
+
+export default MenuItem 
